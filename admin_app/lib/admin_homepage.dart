a213ce27a4ab18ee4ba850fc840dbad54f7c5c83
deeppatel23 <deepforcode23@gmail.com>
2020-12-11 16:10:07 +0530

    Prodcuts can be added

---
+++ admin_app/lib/admin_homepage.dart
@@ -0,0 +1,313 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:admin_app/database/category.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:admin_app/database/add_product.dart';
+
+enum Page { dashboard, manage }
+
+class Admin extends StatefulWidget {
+  @override
+  _AdminState createState() => _AdminState();
+}
+
+class _AdminState extends State<Admin> {
+  Page _selectedPage = Page.dashboard;
+  MaterialColor active = Colors.red;
+  MaterialColor notActive = Colors.grey;
+  TextEditingController categoryController = TextEditingController();
+  GlobalKey<FormState> _categoryFormKey = GlobalKey();
+  CategoryService _categoryService = CategoryService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: <Widget>[
+              Expanded(
+                  child: FlatButton.icon(
+                      onPressed: () {
+                        setState(() => _selectedPage = Page.dashboard);
+                      },
+                      icon: Icon(
+                        Icons.dashboard,
+                        color: _selectedPage == Page.dashboard
+                            ? active
+                            : notActive,
+                      ),
+                      label: Text('Dashboard'))),
+              Expanded(
+                  child: FlatButton.icon(
+                      onPressed: () {
+                        setState(() => _selectedPage = Page.manage);
+                      },
+                      icon: Icon(
+                        Icons.sort,
+                        color:
+                            _selectedPage == Page.manage ? active : notActive,
+                      ),
+                      label: Text('Manage'))),
+            ],
+          ),
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+        ),
+        body: _loadScreen());
+  }
+
+  Widget _loadScreen() {
+    switch (_selectedPage) {
+      case Page.dashboard:
+        return Column(
+          children: <Widget>[
+            FlatButton.icon(
+              onPressed: () {
+                _startAlert();
+              },
+              icon: Icon(
+                Icons.restaurant,
+                size: 30.0,
+                color: Colors.green,
+              ),
+              label: Text('Start',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30.0, color: Colors.green)),
+            ),
+            FlatButton.icon(
+              onPressed: () {
+                _stopAlert();
+              },
+              icon: Icon(
+                Icons.restaurant,
+                size: 30.0,
+                color: Colors.red,
+              ),
+              label: Text('Stop',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30.0, color: Colors.red)),
+            ),
+            Expanded(
+              child: GridView(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                children: <Widget>[
+                  /*Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Card(
+                      child: ListTile(
+                          title: FlatButton.icon(
+                              onPressed: null,
+                              icon: Icon(Icons.people_outline),
+                              label: Text("Users")),
+                          subtitle: Text(
+                            '7',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: active, fontSize: 60.0),
+                          )),
+                    ),
+                  ),*/
+                  Padding(
+                    padding: const EdgeInsets.all(22.0),
+                    child: Card(
+                        child: GestureDetector(
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.category,
+                              size: 50,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "Categories",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                      onTap: () {},
+                    )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(22.0),
+                    child: Card(
+                        child: GestureDetector(
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.emoji_food_beverage,
+                              size: 50,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "Products",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                      onTap: () {},
+                    )),
+                  ),
+                  /*Padding(
+                    padding: const EdgeInsets.all(22.0),
+                    child: Card(
+                      child: ListTile(
+                          title: FlatButton.icon(
+                              onPressed: null,
+                              icon: Icon(Icons.tag_faces),
+                              label: Text("Sold")),
+                          subtitle: Text(
+                            '13',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: active, fontSize: 60.0),
+                          )),
+                    ),
+                  ),*/
+                  Padding(
+                    padding: const EdgeInsets.all(22.0),
+                    child: Card(
+                        child: GestureDetector(
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.shopping_cart,
+                              size: 50,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "Orders",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                      onTap: () {},
+                    )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(22.0),
+                    child: Card(
+                        child: GestureDetector(
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.restaurant,
+                              size: 50,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "Report",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      ),
+                      onTap: () {},
+                    )),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+        break;
+      case Page.manage:
+        return ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.add),
+              title: Text("Add product"),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => AddProduct()));
+              },
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.add_circle),
+              title: Text("Add category"),
+              onTap: () {
+                _categoryAlert();
+              },
+            ),
+          ],
+        );
+        break;
+      default:
+        return Container();
+    }
+  }
+
+  void _categoryAlert() {
+    var alert = new AlertDialog(
+      content: Form(
+        key: _categoryFormKey,
+        child: TextFormField(
+          controller: categoryController,
+          validator: (value) {
+            if (value.isEmpty) {
+              return 'Category cannot be empty';
+            }
+          },
+          decoration: InputDecoration(hintText: "Add category"),
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+            onPressed: () {
+              if (categoryController.text != null) {
+                _categoryService.createCategory(categoryController.text);
+              }
+              Fluttertoast.showToast(msg: 'category created');
+              Navigator.pop(context);
+            },
+            child: Text('ADD')),
+        FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('CANCEL')),
+      ],
+    );
+    showDialog(context: context, builder: (_) => alert);
+  }
+
+  void _stopAlert() {
+    try {
+      FirebaseFirestore.instance
+          .collection('startstop')
+          .doc('nsbLA2tTBDx1N02rwxn6')
+          .update({'value': false});
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void _startAlert() {
+    try {
+      FirebaseFirestore.instance
+          .collection('startstop')
+          .doc('nsbLA2tTBDx1N02rwxn6')
+          .update({'value': true});
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}

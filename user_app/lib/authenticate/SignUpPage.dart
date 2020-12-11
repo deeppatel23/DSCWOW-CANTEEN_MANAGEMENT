4ceac4435d4ddf638d7e6ae58214c3ebb59bb7c3
deeppatel23 <deepforcode23@gmail.com>
2020-12-11 18:18:57 +0530

    user authentication

---
+++ user_app/lib/authenticate/SignUpPage.dart
@@ -0,0 +1,532 @@
+import 'package:user_app/animation/ScaleRoute.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:user_app/authenticate/SignInPage.dart';
+import 'package:user_app/global.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_app/HomePage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+TextEditingController name = new TextEditingController();
+TextEditingController email = new TextEditingController();
+TextEditingController passwd = new TextEditingController();
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+Future<String> signInWithGoogle() async {
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final UserCredential authResult =
+      await _auth.signInWithCredential(credential);
+  final User user = authResult.user;
+
+  if (user != null) {
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final User cUser = _auth.currentUser;
+    assert(user.uid == cUser.uid);
+
+    print('signInWithGoogle succeeded: $user');
+    currentUser = user.displayName;
+    FirebaseFirestore.instance
+        .collection('users')
+        .add({
+          "email": user.email,
+          "orders": [],
+          "name": user.displayName,
+        })
+        .then((result) => {
+              uid = result.id,
+              print("success"),
+            })
+        .catchError((err) => Dialog(child: Text("error" + err)));
+    return '$user';
+  }
+
+  return null;
+}
+
+void createRecord(context) async {
+  print("fuc called");
+  currentUser = name.text;
+  FirebaseFirestore.instance
+      .collection('users')
+      .add({
+        "email": email.text,
+        "orders": [],
+        "name": name.text,
+      })
+      .then((result) => {
+            uid = result.id,
+            print("success"),
+            _register(context),
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            ),
+          })
+      .catchError((err) => Dialog(child: Text("error" + err)));
+}
+
+void _register(context) async {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseUser useraa = (await _auth.createUserWithEmailAndPassword(
+    email: email.text.trim(),
+    password: passwd.text.trim(),
+  ))
+      .user;
+  createRecord(context);
+  if (useraa != null) {
+    print(useraa.toString());
+  } else {
+    print("error");
+  }
+}
+
+// ui
+// =======
+// void createRecord() async {
+//   await Firebase.initializeApp();
+//   print("fuc called");
+//   currentUser = name.text;
+//   FirebaseFirestore.instance
+//       .collection('users')
+//       .add({
+//         "email": email.text,
+//         "orders": [],
+//         "name": name.text,
+//       })
+//       .then((result) => {
+//             uid = result.id,
+//             print("success"),
+//             print(uid),
+//             Dialog(
+//               child: Text("Updated sucessfully"),
+//             ),
+//           })
+//       .catchError((err) => Dialog(child: Text(err)));
+// }
+//main
+
+class SignUpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String defaultFontFamily = 'Roboto-Light.ttf';
+    double defaultFontSize = 14;
+    double defaultIconSize = 17;
+
+// ui
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          padding: EdgeInsets.only(left: 20, right: 20, top: 35, bottom: 30),
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white70,
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                child: InkWell(
+                  child: Container(
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Icon(Icons.close),
+                    ),
+// =======
+                    // return Scaffold(
+                    //   resizeToAvoidBottomInset: false,
+                    //   body: Container(
+                    //     padding: EdgeInsets.only(left: 20, right: 20, top: 30, bottom: 30),
+                    //     width: double.infinity,
+                    //     height: double.infinity,
+                    //     color: Colors.white70,
+                    //     child: Column(
+                    //       children: <Widget>[
+                    //         Flexible(
+                    //           flex: 1,
+                    //           child: InkWell(
+                    //             child: Container(
+                    //               child: Align(
+                    //                 alignment: Alignment.topLeft,
+                    //                 child: Icon(Icons.close),
+//main
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              Flexible(
+                flex: 15,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 230,
+                      height: 100,
+                      alignment: Alignment.center,
+                      child: Image.asset(
+                        "assets/images/menus/ic_food_express.png",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextField(
+                      controller: name,
+                      showCursor: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        filled: true,
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Color(0xFF666666),
+                          size: defaultIconSize,
+                        ),
+                        fillColor: Color(0xFFF2F3F5),
+                        hintStyle: TextStyle(
+                            color: Color(0xFF666666),
+                            fontFamily: defaultFontFamily,
+                            fontSize: defaultFontSize),
+                        hintText: "Name",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextField(
+                      controller: email,
+                      showCursor: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        filled: true,
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Color(0xFF666666),
+                          size: defaultIconSize,
+                        ),
+                        fillColor: Color(0xFFF2F3F5),
+                        hintStyle: TextStyle(
+                            color: Color(0xFF666666),
+                            fontFamily: defaultFontFamily,
+                            fontSize: defaultFontSize),
+                        hintText: "email",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextField(
+                      controller: passwd,
+                      showCursor: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        filled: true,
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Color(0xFF666666),
+                          size: defaultIconSize,
+                        ),
+                        fillColor: Color(0xFFF2F3F5),
+                        hintStyle: TextStyle(
+                            color: Color(0xFF666666),
+                            fontFamily: defaultFontFamily,
+                            fontSize: defaultFontSize),
+                        hintText: "Password",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    SignInButtonWidget(),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("or"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SignInButtonWidget2(),
+                    // FacebookGoogleLogin()
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        child: Text(
+                          "Already have an account? ",
+                          style: TextStyle(
+                            color: Color(0xFF666666),
+                            fontFamily: defaultFontFamily,
+                            fontSize: defaultFontSize,
+                            fontStyle: FontStyle.normal,
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignInPage()),
+                          );
+                        },
+                        child: Container(
+                          child: Text(
+                            "Sign In",
+                            style: TextStyle(
+                              color: Color(0xFFf7418c),
+                              fontFamily: defaultFontFamily,
+                              fontSize: defaultFontSize,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignInButtonWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: new BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Color(0xFFfbab66),
+          ),
+          BoxShadow(
+            color: Color(0xFFf7418c),
+          ),
+        ],
+        gradient: new LinearGradient(
+            colors: [Color(0xFFf7418c), Color(0xFFfbab66)],
+            begin: const FractionalOffset(0.2, 0.2),
+            end: const FractionalOffset(1.0, 1.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp),
+      ),
+      child: MaterialButton(
+          highlightColor: Colors.transparent,
+          splashColor: Color(0xFFf7418c),
+          //shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5.0))),
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 42.0),
+            child: Text(
+              "SIGN UP",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                  fontFamily: "WorkSansBold"),
+            ),
+          ),
+// /<<<<<<< ui
+          onPressed: () => {createRecord(context)}),
+// =======
+      // onPressed: () => {createRecord()}),
+// />>>>>>> main
+    );
+  }
+}
+
+class SignInButtonWidget2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: new BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Color(0xFFfbab66),
+          ),
+          BoxShadow(
+            color: Color(0xFFf7418c),
+          ),
+        ],
+        gradient: new LinearGradient(
+            colors: [Color(0xFFf7418c), Color(0xFFfbab66)],
+            begin: const FractionalOffset(0.2, 0.2),
+            end: const FractionalOffset(1.0, 1.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp),
+      ),
+      child: MaterialButton(
+          highlightColor: Colors.transparent,
+          splashColor: Color(0xFFf7418c),
+          //shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5.0))),
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 32.0),
+            child: Text(
+              "SIGN UP WITH GOOGLE",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontFamily: "WorkSansBold"),
+            ),
+          ),
+// /<<<<<<< ui
+          onPressed: () => {
+                signInWithGoogle().then((result) {
+                  if (result != null) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  }
+                }),
+              }),
+// =======
+      // onPressed: () => {createRecord()}),
+// />>>>>>> main
+    );
+  }
+}
+
+class FacebookGoogleLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  gradient: new LinearGradient(
+                      colors: [
+                        Colors.black12,
+                        Colors.black54,
+                      ],
+                      begin: const FractionalOffset(0.0, 0.0),
+                      end: const FractionalOffset(1.0, 1.0),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.clamp),
+                ),
+                width: 100.0,
+                height: 1.0,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                child: Text(
+                  "Or",
+                  style: TextStyle(
+                      color: Color(0xFF2c2b2b),
+                      fontSize: 16.0,
+                      fontFamily: "WorkSansMedium"),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: new LinearGradient(
+                      colors: [
+                        Colors.black54,
+                        Colors.black12,
+                      ],
+                      begin: const FractionalOffset(0.0, 0.0),
+                      end: const FractionalOffset(1.0, 1.0),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.clamp),
+                ),
+                width: 100.0,
+                height: 1.0,
+              ),
+            ],
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, right: 40.0),
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(15.0),
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFFf7418c),
+                  ),
+                  child: new Icon(
+                    FontAwesomeIcons.facebookF,
+                    color: Color(0xFFFFFFFF),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0),
+              child: GestureDetector(
+                onTap: () => {},
+                child: Container(
+                  padding: const EdgeInsets.all(15.0),
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFFf7418c),
+                  ),
+                  child: new Icon(
+                    FontAwesomeIcons.google,
+                    color: Color(0xFFFFFFFF),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ));
+  }
+}
